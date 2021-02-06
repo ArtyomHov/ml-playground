1095a19aa168b12bc3ebbdbc06496e304ad747cc
Artyom <artyom@ymail.com>
2021-02-06 21:52:50 +0400

    Matplotlib: add example of plots with grids.

---
+++ src/Matplotlib/grids.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+x = np.linspace(-2 * np.pi, 2 * np.pi, 500)
+y = np.sin(x) * np.exp(-x**2/20)
+fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+
+x_major_ticker = mpl.ticker.MultipleLocator(4)
+x_minor_ticker = mpl.ticker.MultipleLocator(1)
+y_major_ticker = mpl.ticker.MultipleLocator(0.5)
+y_minor_ticker = mpl.ticker.MultipleLocator(0.25)
+
+for ax in axes:
+    ax.plot(x, y, lw=2)
+    ax.xaxis.set_major_locator(x_major_ticker)
+    ax.yaxis.set_major_locator(y_major_ticker)
+    ax.xaxis.set_minor_locator(x_minor_ticker)
+    ax.yaxis.set_minor_locator(y_minor_ticker)
+
+axes[0].set_title("default_grid")
+axes[0].grid()
+
+axes[1].set_title("major/minor grid")
+axes[1].grid(color="blue", which="both", linestyle=':', linewidth=0.5)
+
+axes[2].set_title("individual x/y major/minor grid")
+axes[2].grid(color="grey", which="major", axis='x', linestyle='-', linewidth=0.5)
+axes[2].grid(color="grey", which="minor", axis='x', linestyle=':', linewidth=0.25)
+axes[2].grid(color="grey", which="major", axis='y', linestyle='-', linewidth=0.5)
+axes[2].grid(color="grey", which="minor", axis='y', linestyle=':', linewidth=0.25)
+
+plt.show()
